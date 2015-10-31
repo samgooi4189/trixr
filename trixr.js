46a0649a645b61632900f59207c5f8f5c1c5e65b
samgooi4189 <samgooi@gmail.com>
2015-11-01 04:32:13 +0800

    Only owner can see and delete private message.

--- trixr.js
+++ trixr.js
@@ -42,12 +42,21 @@ if (Meteor.isClient) {
     }
   });
 
+  Template.task.helpers({
+    isOwner: function () {
+      return this.owner === Meteor.userId();
+    }
+  });
+
   Template.task.events({
     "click .toggle-checked": function() {
       Meteor.call("setChecked", this._id, !this.checked);
     },
     "click .delete": function () {
       Meteor.call("deleteTask", this._id);
+    },
+    "click .toggle-private": function () {
+      Meteor.call("setPrivate", this._id, !this.private);
     }
   });
 
@@ -71,16 +80,41 @@ Meteor.methods({
     });
   },
   deleteTask: function (taskId) {
+    var task = Task.findOne(taskId);
+    if (task.private && task.owner !== Meteor.userId()) {
+      //If the task is private, make sure only user can delete it
+      throw new Meteor.Error("not-authorized");
+    }
     Tasks.remove(taskId);
   },
   setChecked: function (taskId, setChecked) {
+    var task = Task.findOne(taskId);
+    if (task.private && task.owner !== Meteor.userId()) {
+      //If the task is private, make sure only user can delete it
+      throw new Meteor.Error("not-authorized");
+    }
     Tasks.update(taskId, { $set: { checked: setChecked}});
+  },
+  setPrivate: function (taskId, setToPrivate) {
+    var task = Tasks.findOne(taskId);
+
+    //Make sure only task owner can make a task private
+    if (task.owner !== Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+
+    Tasks.update(taskId, { $set: {private: setToPrivate }});
   }
 });
 
 if (Meteor.isServer) {
   // code to run on server at startup
   Meteor.publish("tasks", function() {
-    return Tasks.find();
+    return Tasks.find({
+      $or: [
+        { private: {$ne: true} },
+        { owner: this.userId }
+      ]
+    });
   });
 }
